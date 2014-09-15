eedc7bec9b3dfeb7cd6dc072f2d9966eb13716c6
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2014-09-14 23:59:53 -0400

    forms for website registration, match reporting

---
+++ uchicagosmash/powerranking/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from uchicagosmash.powerranking.models import *
+
+class RegistrationForm(forms.ModelForm):
+	class Meta:
+		model = Smasher
+		fields = ('tag', 'dorm', 'agree')
+
+	agree = forms.BooleanField()
+
+	def __init__(self, *args, **kwargs):
+		super(GameRegistrationForm, self).__init__(*args, **kwargs)
+		self.fields['dorm'].widget.attrs['class'] = 'form-control' # for Bootstrap 3
+		self.fields['dorm'].widget.attrs['required'] = 'required'
+		self.fields['tag'].widget.attrs['required'] = 'required'
+		self.fields['agree'].widget.attrs['required'] = 'required'
+
+
+class MatchForm(forms.ModelForm):
+	class Meta:
+		model = Match
+		fields = ('winner', 'loser')
+
+	def __init__(self, *args, **kwargs):
+		super(MatchForm, self).__init__(*args, **kwargs)
+		self.fields['winner'].widget.attrs['required'] = 'required'
+		self.fields['loser'].widget.attrs['required'] = 'required'
+
+
+class MatchVerificationForm(forms.ModelForm):
+	class Meta:
+		model = Match
+		fields = ('verified')
+
+	def __init__(self, *args, **kwargs):
+		super(MatchVerificationForm, self).__init__(*args, **kwargs)
+		self.fields['verified'].widget.attrs['required'] = 'required'
