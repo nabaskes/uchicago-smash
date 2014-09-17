386c44ea3a40ff20714ef698843a83a6b0ae51f9
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2014-09-16 22:03:52 -0400

    elo calculation functions

---
+++ uchicagosmash/powerranking/elo.py
@@ -0,0 +1,13 @@
+from uchicagosmash.powerranking.models import *
+
+def expected_value(ra, rb):
+	return 1/(1 + pow(10, (rb - ra)/400))
+
+def calculate_elo(winner, loser):
+	winner_expected = expected_value(winner, loser)
+	loser_expected = expected_value(loser, winner)
+
+	winner_new = winner + K*(1 - winner_expected)
+	loser_new = loser - (K*loser_expected)
+
+	return [winner_new, loser_new]
