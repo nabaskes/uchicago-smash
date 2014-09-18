1b3ca6959a07972233261cc435c0713af4e8be1e
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2014-09-17 23:05:55 -0400

    fixed context processor error when user not authenticated and smasher doesn't exist

--- uchicagosmash/context_proccessors.py
+++ uchicagosmash/context_proccessors.py
@@ -2,12 +2,16 @@ from uchicagosmash.powerranking.models import *
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 
-@login_required
+
 def notification(request):
-	unverified_matches = Match.objects.filter(Q(winner__user=request.user) | Q(loser__user=request.user), verified=False).count()
-	smasher = Smasher.objects.filter(user=request.user)
-	if smasher:
-		tag = smasher[0].tag
+	if request.user.is_authenticated():
+		unverified_matches = Match.objects.filter(Q(winner__user=request.user) | Q(loser__user=request.user), verified=False).count()
+		smasher = Smasher.objects.filter(user=request.user)
+		if smasher:
+			tag = smasher[0].tag
+		else:
+			tag = ''
 	else:
+		unverified_matches = 0
 		tag = ''
 	return {'notifications':unverified_matches, 'tag':tag}
