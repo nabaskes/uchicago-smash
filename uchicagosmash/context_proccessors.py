2ab9435e992c7d59eaa99dc561cdcb7dbc6d5c50
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2014-09-18 01:34:19 -0400

    register only appears if user isn't registered, user's smasher is in global context

--- uchicagosmash/context_proccessors.py
+++ uchicagosmash/context_proccessors.py
@@ -6,12 +6,15 @@ from django.contrib.auth.decorators import login_required
 def notification(request):
 	if request.user.is_authenticated():
 		unverified_matches = Match.objects.filter(Q(winner__user=request.user) | Q(loser__user=request.user), verified=False).exclude(submitter__user=request.user).count()
-		smasher = Smasher.objects.filter(user=request.user)
-		if smasher:
-			tag = smasher[0].tag
+		query = Smasher.objects.filter(user=request.user)
+		if query:
+			tag = query[0].tag
+			smasher = query[0]
 		else:
+			smasher = None
 			tag = ''
 	else:
 		unverified_matches = 0
 		tag = ''
-	return {'notifications':unverified_matches, 'tag':tag}
+		smasher = None
+	return {'notifications':unverified_matches, 'tag':tag, 'smasher':smasher}
