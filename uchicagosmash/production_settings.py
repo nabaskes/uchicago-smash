ca60907ba94285c71638488562d5868cbef78d6b
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2014-09-24 16:31:42 -0400

    production settings stuff

---
+++ uchicagosmash/production_settings.py
@@ -0,0 +1,42 @@
+from local_settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['uchicagosmash.com', 'www.uchicagosmash.com']
+
+ADMINS = (
+    ('Administrator', 'admin@uchicagosmash.com'),
+)
+SERVER_EMAIL = 'noreply@uchicagosmash.com'
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'uchicagosmashdb',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# REST framework settings
+REST_FRAMEWORK = {
+	'DEFAULT_RENDERER_CLASSES': (
+		'rest_framework.renderers.JSONRenderer',
+	)
+}
+
+# Email settings
+EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
+CELERY_EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+## Uncomment for Mandrill
+from secrets import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+EMAIL_HOST = 'smtp.mandrillapp.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+
+## Uncomment for localhost email
+#EMAIL_HOST = 'localhost'
