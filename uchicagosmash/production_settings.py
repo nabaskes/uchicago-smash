c5c476831e430c4c9ff61ec7fd03f1daea685aa5
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2014-09-28 16:49:22 -0400

    got rid of heroku stuff

--- uchicagosmash/production_settings.py
+++ uchicagosmash/production_settings.py
@@ -1,11 +1,5 @@
 from local_settings import *
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 DEBUG = False
 
 ALLOWED_HOSTS = ['uchicagosmash.com', 'www.uchicagosmash.com']
